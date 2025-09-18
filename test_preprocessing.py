f6128d87a8760e9a90931b0ee1b69fc739c1fff8
casiglia <matilde.casiglia@polimi.it>
2025-09-19 01:19:14 +0200

    modify correct directory for the images

--- test_preprocessing.py
+++ test_preprocessing.py
@@ -1,8 +1,13 @@
 from PIL import Image
-from preprocessing.resize import preprocess  # Cambia qui il metodo da testare
+from preprocessing.pad import preprocess  # Cambia qui il metodo da testare
+import os
 
-# Percorso corretto dell'immagine nella cartella 'immagini' del progetto
-img_path = "C:/Users/matilde/Desktop/PROGETTI/tactile_anomaly_detection/immagini/image1.jpg""
+folder = "C:/Users/matilde/Desktop/PROGETTI/tactile_anomaly_detection/immagini"
+print("File presenti nella cartella immagini:")
+print(os.listdir(folder))
+
+# Percorso corretto del file immagine (non della cartella)
+img_path = "C:/Users/matilde/Desktop/PROGETTI/tactile_anomaly_detection/immagini/image1.PNG"
 
 img = Image.open(img_path)
 img_proc = preprocess(img)
